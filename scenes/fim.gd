4a184b3aa4c7e6749ac51244413dca4101875b9d
Rafael Duarte <122548782+ssparkyss@users.noreply.github.com>
2026-03-03 13:36:08 +0000

    Delete scenes directory

--- scenes/fim.gd
+++
@@ -1,6 +0,0 @@
-extends Node2D
-
-func _on_back_button_pressed() -> void:
-	$MenuSound.play()
-	await $MenuSound.finished
-	get_tree().change_scene_to_file("res://scenes/main_menu.tscn")
--- scenes/levels/2.gd
+++
@@ -1,7 +0,0 @@
-extends Node2D
-
-var level: int = 1
-
-func _on_kim_inimigo_tree_exited() -> void:
-	$Morte.play()
-	$LevelFinishOrb.visible = true
